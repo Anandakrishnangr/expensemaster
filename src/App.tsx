0569438f17e050495d78ddd8d4389748b884c295
ananda krishnan gr <granandakrishnan@gmail.com>
2024-07-27 21:13:48 +0530

    sp

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Paper } from '@mui/material';
 import RouteMap from './RouteMap';
-import {SnackBar} from './_components/index';
-
+import { SnackBar } from './_components';
+import './App.css'
 const queryClient = new QueryClient();
 const App: React.FC = () => {
   return (
