96962d2f6d97b67a9cf3e3a0ce3c6c416243b6c3
ananda krishnan gr <granandakrishnan@gmail.com>
2024-07-28 17:37:34 +0530

    c

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,24 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import authReducer from './authSlice'
-import modalSlice from './modalSlice';
+import authReducer from './authSlice';
+import dataReducer from './dataSlice';
+import modalReducer from './modalSlice';
+
+// Configuration for the persisted reducers
 const persistConfig = {
-    key: 'root',
+    key: 'root', // The key for the persisted state
     storage,
-    whitelist: ['modalSlice'],
+    whitelist: ['auth', 'data'], // Only the 'auth' and 'data' slices will be persisted
 };
 
-const persistedReducer = persistReducer(persistConfig, authReducer);
-
-export const store = configureStore({
-    reducer: {
-        auth: persistedReducer,
-        modal: modalSlice
-    },
+// Create a persisted reducer for the auth and data slices
+const rootReducer = combineReducers({
+    auth: authReducer,
+    data: dataReducer,
+    modal: modalReducer, // Modal slice is not persisted
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+export const store = configureStore({
+    reducer: persistedReducer,
+});
 
 // Correctly type 'persistor'
 export type RootState = ReturnType<typeof store.getState>;
